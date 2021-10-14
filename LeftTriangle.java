5799cb27bfc64ce67b9ec8875fddf9bf41fb86f0
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-10-14 13:42:38 +0530

    Add files via upload

---
+++ LeftTriangle.java
@@ -0,0 +1,40 @@
+package patternPrint;
+
+public class LeftTriangle {
+
+	public static void main(String[] args) {
+		  int row=8;
+	      int i,j;
+	      int mid =0;
+	      mid =row-1;
+	      int n=1;
+	      
+	      for(i=row;i>0;i--) {	    	 
+	    	  for(j=0;j<mid;j++) {
+	    		  System.out.print(" ");    
+	    	  }	    	 
+	    	  if(n<=row) {
+	    		 for(i=1;i<=n;i++) {
+	    			 System.out.print("*"); 
+	    		 } 
+	    	  }
+	    	  System.out.println("");
+	    	  mid-=1;
+	    	  n =n+1;
+	      }
+		}
+}
+
+//sample output
+/*
+       *
+      **
+     ***
+    ****
+   *****
+  ******
+ *******
+********  
+
+  
+*/
