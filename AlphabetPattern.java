ba3d7c3a8398d0183fe11166006eb2e7dbdf08ac
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-10-17 19:35:47 +0530

    Add files via upload

---
+++ AlphabetPattern.java
@@ -0,0 +1,22 @@
+package patternPrint;
+
+public class AlphabetPattern {
+
+	public static void main(String[] args) {
+		int n=8;
+		
+		for(int i=0;i<=8;i++) {
+			for(int j=0;j<=n/2;j++) {
+				if((j==0 || j==n/2)&& i!=0 ||i==0 && j!=n/2 || i==n/2 ){
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
