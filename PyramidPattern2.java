b67db59279f6bf1c824753d5947032729a127894
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-10-19 19:15:06 +0530

    Add files via upload

---
+++ PyramidPattern2.java
@@ -0,0 +1,40 @@
+package patternPrint;
+
+import java.util.Scanner;
+
+public class PyramidPattern2 {
+
+	public static void main(String[] args) {
+		 int row;
+         Scanner in = new Scanner(System.in);
+         System.out.print("Enter the row : ");
+         row = in.nextInt();
+         int Space =row-1;
+         for(int i=0;i<row;i++) {
+        	 for(int j=0;j<Space;j++) {
+        		 System.out.print(" ");
+        	 }
+        	 Space--;
+        	 for(int k=0;k<=i;k++) {
+        		 System.out.print("*" + " ");
+        	 }
+        	 System.out.println(" ");
+         }
+
+	}
+
+}
+
+//Sample output
+
+/*
+
+Enter the row : 6
+     *  
+    * *  
+   * * *  
+  * * * *  
+ * * * * *  
+* * * * * *  
+
+*/
