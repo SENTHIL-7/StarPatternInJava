0a23efaf41ead9a0269ba68ca34795a305d19b54
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-10-20 13:21:17 +0530

    Add files via upload

---
+++ DownwardTriangleStarPattern.java
@@ -0,0 +1,22 @@
+package patternPrint;
+
+import java.util.Scanner;
+public class DownwardTriangleStarPattern {
+
+	public static void main(String[] args) {
+		
+         int row;
+         Scanner in = new Scanner(System.in);
+         System.out.print("Enter the row : ");
+         row = in.nextInt();
+         int col =row;
+         for(int i=0;i<row;i++) {
+        	 for(int j=0;j<col;j++) {
+        		 System.out.print("*");
+        	 }
+        	 col--;
+        	 System.out.println("");
+         }
+	}
+
+}
