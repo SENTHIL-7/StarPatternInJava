2acff5fc3bf7041b291c6a013fee161da5829c37
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-10-23 19:30:16 +0530

    Add files via upload

---
+++ TriangleStarPattern.java
@@ -0,0 +1,42 @@
+package patternPrint;
+
+public class TriangleStarPattern {
+
+	public static void main(String[] args) {
+		
+		int row=9;
+		for(int i=1;i<=row;i++) {
+
+			for(int j=i;j<row;j++) {
+                System.out.print(" ");   
+			}
+			for(int k=1;k<=2*i-1;k++) {
+				if(k==1||i==row||k==2*i-1) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
+
+//sample output
+
+/*
+
+        *
+       * *
+      *   *
+     *     *
+    *       *
+   *         *
+  *           *
+ *             *
+*****************
+
+*/
